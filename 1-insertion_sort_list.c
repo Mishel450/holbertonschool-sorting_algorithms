19eda7fdce83988c72fd592c7da45a608d25cb40
Mishel450 <tomasmishel18@gmai.com>
2023-05-18 12:08:00 -0700

    sorting_algorithms

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts the doubly linked list.
+ * @list: the doubly linked list.
+ * Return: noting.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *t, *n, *aux;
+	
+	if (list != NULL)
+	{
+		aux = (*list);
+		while (aux->next)
+		{
+			while (aux->n > aux->next->n)
+			{
+				t = aux;
+				n = aux->next;
+				n->prev = t->prev;
+				if (t->prev != NULL)
+				t->prev->next = n;
+				else
+				*list = n;
+				if (n->next != NULL)
+				n->next->prev = t;
+				t->next = n->next;
+				t->prev = n;
+				n->next = t;
+				print_list(*list);
+				if (n->prev != NULL)
+				aux = n->prev;
+			}
+			aux = aux->next;
+		}
+	}
+}
